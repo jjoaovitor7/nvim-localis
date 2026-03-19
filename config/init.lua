7f1164861f09b382d77557494b0abbc16787c056
João Vítor <jjoaovitor7@outlook.com.br>
2026-03-19 18:02:29 -0300

    feat: update 'init.lua'.

--- config/init.lua
+++ config/init.lua
@@ -1,12 +1,12 @@
 vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46/"
 vim.g.mapleader = " "
 
--- bootstrap lazy and all plugins
+-- bootstrap lazy and all plugins:
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.uv.fs_stat(lazypath) then
-	local repo = "https://github.com/folke/lazy.nvim.git"
-	vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -15,39 +15,51 @@ local lazy_config = require("configs.lazy")
 
 -- load plugins
 require("lazy").setup({
-	{
-		"NvChad/NvChad",
-		lazy = false,
-		branch = "v2.5",
-		import = "nvchad.plugins",
-	},
-	{
-		"stevearc/conform.nvim",
-		opts = {},
-	},
-	{
-		"uhs-robert/sshfs.nvim",
-		opts = {
-			ssh_configs = {
-				"~/.ssh/config",
-				"/etc/ssh/ssh_config",
-			},
-		},
-	},
-	{
-		"rest-nvim/rest.nvim",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			highlight = {
-				enable = true,
-			},
-			opts = function(_, opts)
-				opts.ensure_installed = opts.ensure_installed or {}
-				table.insert(opts.ensure_installed, "http")
-			end,
-		},
-	},
-	{ import = "plugins" },
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {},
+  },
+  {
+    "hrsh7th/nvim-cmp",
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    -- :Lazy build markdown-preview.nvim
+  },
+  {
+    "uhs-robert/sshfs.nvim",
+    opts = {
+      ssh_configs = {
+        "~/.ssh/config",
+        "/etc/ssh/ssh_config",
+      },
+    },
+  },
+  {
+    "rest-nvim/rest.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      highlight = {
+        enable = true,
+      },
+      opts = function(_, opts)
+        opts.ensure_installed = opts.ensure_installed or {}
+        table.insert(opts.ensure_installed, "http")
+      end,
+    },
+  },
+  { import = "plugins" },
 }, lazy_config)
 
 -- load theme
@@ -58,22 +70,55 @@ require("options")
 require("autocmds")
 
 vim.schedule(function()
-	require("mappings")
+  require("mappings")
 end)
 
 vim.opt.list = true
 vim.opt.listchars = {
-	space = "·",
-	tab = "▶ ",
-	trail = "·",
-	eol = "¬",
+  space = "·",
+  tab = "▶ ",
+  trail = "·",
+  eol = "¬",
 }
 
 require("conform").setup({
-	formatters_by_ft = {},
-	format_on_save = {
-		-- These options will be passed to conform.format()
-		timeout_ms = 512,
-		lsp_format = "never",
-	},
+  formatters_by_ft = {
+    go = {
+      -- go install golang.org/x/tools/cmd/goimports@latest
+      "goimports"
+    },
+    js = {
+      -- npm i -g prettier
+      "prettier"
+    },
+    css = {
+      "prettier"
+    },
+    lua = {
+      "stylua"
+    },
+  },
+  format_on_save = function(bufnr)
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+    return { timeout_ms = 512, lsp_format = "never" }
+  end,
+})
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
 })
